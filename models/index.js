6b712515256c7dd9ec0ba37af4135d88e0f16ff4
lester <jodra@stratpoint.com>
2020-03-31 18:16:27 +0800

    Connect to database

---
+++ models/index.js
@@ -0,0 +1,20 @@
+
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('inventory', 'root', 'root', {
+  host: 'localhost',
+  dialect: 'mysql',
+  pool: {
+    max: 10,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+});
+
+sequelize.sync({ force: true }).then(() => {
+    console.log(`Database & tables created!`)
+});
+
+module.exports = {
+}
