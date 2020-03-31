ccf28dd8c1041fe07fd84f245bd53d65494ba0fe
lester <jodra@stratpoint.com>
2020-03-31 19:40:28 +0800

    Added body parse middleware

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 const item = require('./routes/item');
 
+app.use(bodyParser());
 app.use('/api/items', item);
 
 module.exports = app;
