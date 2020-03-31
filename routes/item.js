70f7c7f67964e2bf17ee7f59c69cefa55025aa79
lester <jodra@stratpoint.com>
2020-03-31 19:41:39 +0800

    Handle item routing

--- routes/item.js
+++ routes/item.js
@@ -2,33 +2,45 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
+const Item = new (require('../controllers/ItemController'))();
+
+router.get('/', async (req, res, next) => {
+  const items = await Item.list();
+
   return res.json({
-    //
+    data: items,
   });
 });
 
-router.get('/:id', (req, res, next) => {
+router.get('/:id', async (req, res, next) => {
+  const { id } = req.params;
+  const item = await Item.find(id);
   return res.json({
-    //
+    data: item,
   });
 });
 
-router.post('/', (req, res, next) => {
+router.post('/', async (req, res, next) => {
+  const { body } = req;
+  const newItem = await Item.create(body);
   return res.json({
-    //
+    data: newItem,
   });
 });
 
-router.put('/:id', (req, res, next) => {
+router.put('/:id', async (req, res, next) => {
+  const { body, params: { id } } = req;
+  const item = await Item.update(id ,body);
   return res.json({
-    //
+    data: item
   });
 });
 
 router.delete('/:id', (req, res, next) => {
+  const { id } = req.params;
+  Item.delete(id);
   return res.json({
-    //
+    data: 'deleted',
   });
 });
 
